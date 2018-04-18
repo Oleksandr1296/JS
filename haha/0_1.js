a6ba6ab210711e79f34cb443d48852a6a2e244b0
Oleksandr1296 <olkravchukv@gmail.com>
2018-04-18 12:08:33 +0300

    add intarface

---
+++ haha/0_1.js
@@ -0,0 +1,6 @@
+function LogNumbrs(i){
+	for (i; i > 0; i--){
+		console.log(i);
+	}
+}
+LogNumbrs (10);
--- haha/1/0_1.js
+++
@@ -1,7 +0,0 @@
-function LogNumbrs(i) {
-    for (i; i > 0; i--)
-    {
-        console.log(i);
-    }
-}
-LogNumbrs (10);
